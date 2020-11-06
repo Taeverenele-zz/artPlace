97a8651d4877a5f8f115ef3a81a2986f2958bc7e
Nele Taevere <ntaevere@gmail.com>
2020-11-06 10:34:22 +1000

    added rolify and created users in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+user_1 = User.create!(email: ENV['USER_1_EMAIL'], password: ENV['USER_1_PASSWORD'])
+user_2 = User.create!(email: ENV['USER_2_EMAIL'], password: ENV['USER_2_PASSWORD'])
+admin_user = User.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'])
+admin_user.add_role :admin
