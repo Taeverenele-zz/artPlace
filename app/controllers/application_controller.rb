c0a287da8ced95b1f8dbf0dd5dfe323a9d397a30
Nele Taevere <ntaevere@gmail.com>
2020-11-06 14:29:41 +1000

    added CanCanCan and added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  # display error message when user tried to access unauthorized page
+rescue_from CanCan::AccessDenied do |exception|
+  redirect_to root_path, notice: "You are not authorized to access this page"
+end
 end
--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -20,6 +20,7 @@ class ArtworksController < ApplicationController
 
   # GET /artworks/1/edit
   def edit
+    authorize! :manage, @artwork
   end
 
   # POST /artworks
@@ -42,6 +43,7 @@ class ArtworksController < ApplicationController
   # PATCH/PUT /artworks/1
   # PATCH/PUT /artworks/1.json
   def update
+    authorize! :manage, @artwork
     respond_to do |format|
       if @artwork.update(artwork_params)
         format.html { redirect_to @artwork, notice: 'Artwork was successfully updated.' }
@@ -56,6 +58,7 @@ class ArtworksController < ApplicationController
   # DELETE /artworks/1
   # DELETE /artworks/1.json
   def destroy
+    authorize! :manage, @artwork
     @artwork.destroy
     respond_to do |format|
       format.html { redirect_to artworks_url, notice: 'Artwork was successfully destroyed.' }
