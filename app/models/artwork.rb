7473185cf829a5185452a54dee6d53834b63f16c
Nele Taevere <ntaevere@gmail.com>
2020-11-10 10:58:10 +1000

    added sales model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,6 +2,7 @@ class Artwork < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   belongs_to :user
+  has_many :sales
   has_one_attached :image, dependent: :destroy
   validates :image, attached: true
 end
