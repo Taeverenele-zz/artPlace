42c220f1023fd79311ca34372003fffa16258848
Nele Taevere <ntaevere@gmail.com>
2020-11-12 16:53:23 +1000

    added validation to new artwork form

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,7 +2,9 @@ class Artwork < ApplicationRecord
   belongs_to :user
   has_many :sales, dependent: :destroy
   has_one_attached :image, dependent: :destroy
+  validates :title, length: {minimum: 3, maximum: 20}
+  validates :description, length: {minimum: 10, maximum: 200}
   validates :image, attached: true
-  validates :price, numericality: {greater_than: 49}
+  validates :price, numericality: {greater_than: 49, message: " must be greater than 49 cents"}
   attribute :availability, default: true
 end
